6dc637848fe0dca43e2f438e243a209b1710eec5
Jeff L <jeffreyliu97@gmail.com>
2023-03-28 11:40:22 -0700

    added nav and pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  HomePage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 };
---
+++ app/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar";
+export { default as HomePage } from "./HomePage";
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
+export { default as CryptoDetails } from "./Navbar";
