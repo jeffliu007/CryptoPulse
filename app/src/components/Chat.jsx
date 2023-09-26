360590ddb72a0d53a5e0d39edac32743f096bf25
Jeff L <jeffreyliu97@gmail.com>
2023-09-26 14:38:11 -0700

    fixed chat component

--- app/src/components/Chat.jsx
+++ app/src/components/Chat.jsx
@@ -2,64 +2,66 @@ import { useState, useEffect } from "react";
 import openai from "../services/openAI.js";
 
 const Chat = () => {
-	const [messages, setMessages] = useState([]);
-	const [inputMessage, setInputMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
 
-	const sendMessage = async () => {
-		if (inputMessage.trim() === "") return;
+  const sendMessage = async () => {
+    if (inputMessage.trim() === "") return;
 
-		// Add the user's message to the list of messages
-		setMessages([...messages, { role: "user", content: inputMessage }]);
+    setMessages([...messages, { role: "user", content: inputMessage }]);
 
-		try {
-			const chatCompletion = await openai.chat.completions.create({
-				model: "gpt-3.5-turbo",
-				messages: [...messages, { role: "user", content: inputMessage }],
-			});
+    try {
+      const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [...messages, { role: "user", content: inputMessage }],
+      });
 
-			// Extract the content from the API response and add it to the list of messages
-			const assistantReply = chatCompletion.choices[0].message.content;
-			setMessages([
-				...messages,
-				{ role: "assistant", content: assistantReply },
-			]);
-		} catch (error) {
-			console.error("Error fetching data:", error);
-		}
+      const assistantReply = chatCompletion.choices[0].message.content;
+      setMessages([
+        ...messages,
+        { role: "assistant", content: assistantReply },
+      ]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
 
-		// Clear the input field after sending the message
-		setInputMessage("");
-	};
+    setInputMessage("");
+  };
+  const handleKeyDown = (e) => {
+    if (e.code === "Enter") {
+      sendMessage(e.target.value);
+      setInputMessage("");
+    }
+  };
 
-	useEffect(() => {
-		// Initialize the chat with a welcome message
-		setMessages([
-			{ role: "system", content: "Welcome to the chat!" },
-			...messages,
-		]);
-	}, []); // This effect runs once when the component mounts
+  useEffect(() => {
+    setMessages([{ role: "system", content: "" }, ...messages]);
+  }, []);
 
-	return (
-		<div>
-			<h1>Chat Room</h1>
-			<div className="chat-container">
-				{messages.map((message, index) => (
-					<div key={index} className={`message ${message.role}`}>
-						{message.content}
-					</div>
-				))}
-			</div>
-			<div className="input-container">
-				<input
-					type="text"
-					value={inputMessage}
-					onChange={(e) => setInputMessage(e.target.value)}
-					placeholder="Type your message..."
-				/>
-				<button onClick={sendMessage}>Send</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="chat-wrapper">
+      <h1>Ask GPT Any Questions!</h1>
+      <div className="chat-container">
+        {messages.map((message, index) => (
+          <div key={index} className="message-list">
+            {message.content}
+          </div>
+        ))}
+      </div>
+      <div className="input-container">
+        <textarea
+          type="text"
+          value={inputMessage}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => setInputMessage(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button className="chat-button" onClick={sendMessage}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
